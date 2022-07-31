42017d622a5393e9fd9a90253c2d03b9121b3985
Sszyh <zhuyer@outlook.com>
2022-07-30 17:08:52 -0700

    delete console.log and irrelevant function

--- index.js
+++ index.js
@@ -1,75 +1,19 @@
-const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes, nextISSTimesForMyLocation } = require('./iss');
-
-// fetchMyIP((error, ip) => {
-
-//   if (error) {
-//     console.log("It didn't work!" , error);
-//     return;
-//   }
-//   console.log('It worked! Returned IP:' , ip);
-// });
-
-// fetchCoordsByIP("16.16.160.16",(error, data) => {
-//     //ip is fake
-//   if (error) {
-//     console.log(error);
-//     return;
-//   }
-
-//   const { latitude, longitude } = data;
-//     //let latitude = data.latitude
-//     //let longtitude = data.longitude
-
-//   console.log({latitude, longitude});
-
-// });
-
-// fetchISSFlyOverTimes({ latitude: 49.2827291, longitude: -123.1207375 }, (error, data) => {
-//   if(error) {
-//     console.log(error);
-//     return;
-//   }
-  
-//   console.log(data.response);
-// });
+const { nextISSTimesForMyLocation } = require('./iss');
 
 
+const printPassTimes = (passTimes) => {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+}
 
 nextISSTimesForMyLocation((error, passTimes) => {
   if (error) {
     return console.log("It didn't work!", error);
   }
   // success, print out the deets!
-  //console.log("pass",passTimes);
-  //passTimes.response
-  for (const pass of passTimes.response) {
-    const datetime = new Date(0);
-    datetime.setUTCSeconds(pass.risetime);
-    const duration = pass.duration;
-    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
-  //console.log(passTimes);
-  }
-  
+  printPassTimes(passTimes);
 });
-
-
-
-
-
-// const printPassTimes = function(passTimes) {
-//   console.log("pass",passTimes);
-//   for (const pass of passTimes) {
-//     const datetime = new Date(0);
-//     datetime.setUTCSeconds(pass.risetime);
-//     const duration = pass.duration;
-//     console.log(`Next pass at ${datetime} for ${duration} seconds!`);
-//   }
-// };
-
-// nextISSTimesForMyLocation((error, passTimes) => {
-//   if (error) {
-//     return console.log("It didn't work!", error);
-//   }
-//   // success, print out the deets!
-//   printPassTimes(passTimes);
-// });
